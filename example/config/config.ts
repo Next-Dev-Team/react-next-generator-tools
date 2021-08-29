2e4051f32758c9508c3ff3ade8dab0c99a15b00b
sial <rimsila.itc@gmail.com>
2021-08-29 22:17:26 +0700

    update dp

--- example/config/config.ts
+++ example/config/config.ts
@@ -1,8 +1,19 @@
+import AntdDayjsWebpackPlugin from 'antd-dayjs-webpack-plugin';
 import { defineConfig } from 'umi';
 import routeConfig from './routeConfig';
 
 export default defineConfig({
   routes: routeConfig,
   mfsu: {},
+  dynamicImport: {
+    loading: '@ant-design/pro-layout/es/PageLoading',
+  },
   esbuild: {},
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  chainWebpack(config) {
+    config.plugin('dayjs').use(AntdDayjsWebpackPlugin);
+    return config;
+  },
 });
